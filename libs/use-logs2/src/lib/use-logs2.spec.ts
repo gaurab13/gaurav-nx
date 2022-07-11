3f0158761dd4de82489871e7eb361f1f5532ce82
Gaurav KC <13gaurab@gmail.com>
2022-07-11 16:50:35 +0700

    Publish v 3

--- libs/use-logs2/src/lib/use-logs2.spec.ts
+++ libs/use-logs2/src/lib/use-logs2.spec.ts
@@ -2,6 +2,6 @@ import { useLogs2 } from './use-logs2';
 
 describe('useLogs2', () => {
   it('should work', () => {
-    expect(useLogs2()).toEqual('use-logs2');
+    expect(useLogs2('text')).toEqual('Logs: text');
   });
 });
--- libs/use-logs2/src/lib/use-logs2.ts
+++ libs/use-logs2/src/lib/use-logs2.ts
@@ -1,3 +1,3 @@
-export function useLogs2(): string {
-  return 'use-logs2';
+export function useLogs2(log: string): string {
+  return `Logs: ${log}`;
 }
